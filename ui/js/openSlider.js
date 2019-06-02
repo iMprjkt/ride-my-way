3df97cf2ce368e06b3f3706baaa5aec9abe4144b
iMprojekt <imatafu@outlook.com>
2019-06-03 00:27:01 +0300

    completed one page html app

--- ui/js/openSlider.js
+++ ui/js/openSlider.js
@@ -1,7 +1,15 @@
 function openNav() {
-    document.getElementById("userProfile").style.width = "100%";
-  }
+  document.getElementById("userProfile").style.width = "100%";
+}
 
-  function closeNav() {
-    document.getElementById("userProfile").style.width = "0";
-  }
+function closeNav() {
+  document.getElementById("userProfile").style.width = "0";
+}
+
+function openSld() {
+  document.getElementById("destinationSlider").style.height = "100%";
+}
+
+function closeSld() {
+  document.getElementById("destinationSlider").style.height = "20%";
+}
