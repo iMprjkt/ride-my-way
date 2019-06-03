a3f7e14d210c33d7120b44807b702a47998199fa
iMprojekt <imatafu@outlook.com>
2019-06-03 21:07:02 +0300

    folder structure and initial react cleanup

--- frontend/src/App/App.js
+++ frontend/src/App/App.js
@@ -1,12 +1,1 @@
-import React from 'react';
 
-const App=()=> {
-
-        return (
-            <div className="App">
-                <h1>Hi</h1>
-            </div>
-        );
-    }
-
-export default App;
